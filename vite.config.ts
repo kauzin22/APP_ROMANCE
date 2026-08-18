27f7d67c7ae938f4a5c31dbd3218d92beda278c1
Kaue Lennon <kauelennon@Kaues-Mac-mini.local>
2026-08-18 09:13:59 -0300

    fix: atualizando index.html completo e estaticos

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    host: '0.0.0.0',
-    port: 5173
-  }
+  base: './', // <-- Faz o Vite usar caminhos relativos em todo o build
 })
